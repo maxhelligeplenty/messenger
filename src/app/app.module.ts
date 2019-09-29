a6dcc8e3d23ca7f426cc083086dcd35ea0e469a6
Maurizio9816 <maurizio.crescente@yahoo.com>
2019-09-29 18:22:40 +0200

    container

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,15 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { MessageRoomComponent } from './views/message-room/message-room.component';
 import { ChatBoxComponent } from './components/chat-box/chat-box.component';
+import { ContainerComponent } from './components/container/container.component';
 import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     MessageRoomComponent,
-    ChatBoxComponent
+    ChatBoxComponent,
+    ContainerComponent
   ],
   imports: [
     BrowserModule,
