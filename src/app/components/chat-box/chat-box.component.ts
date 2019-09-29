d6bdd7f075732f277b45af6f527d8ab18b0bce3c
Maurizio9816 <maurizio.crescente@yahoo.com>
2019-09-29 17:11:26 +0200

    text-color

--- src/app/components/chat-box/chat-box.component.ts
+++ src/app/components/chat-box/chat-box.component.ts
@@ -10,6 +10,9 @@ export class ChatBoxComponent
   protected message:string;
   protected messages:Array<string> = [];
 
+  protected message2:string;
+  protected messages2:Array<string> = [];
+
   public sendMessage(message:string):void {
     if(message === undefined || message === null || this.messages === null || this.messages === undefined){
         return;
@@ -17,6 +20,11 @@ export class ChatBoxComponent
     this.messages.push(message);
   }
 
+  public sendMessage2(message2:string):void {
+    if(message2 === undefined || message2 === null || this.messages2 === null || this.messages2 === undefined){
+        return;
+    }
+    this.messages2.push(message2);
+  }
+
 }
-
-
